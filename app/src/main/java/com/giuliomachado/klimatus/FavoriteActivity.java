a525f9f30ae45b1e1e98a6bf7b15804e8b0a3010
Giuliomachado <giuliocfmachado@gmail.com>
2020-04-27 14:00:50 -0300

    Versão 1.2 Criação da tela de Cidades Favoritas Criação do cabeçalho Modelagem das listas

--- app/src/main/java/com/giuliomachado/klimatus/FavoriteActivity.java
+++ app/src/main/java/com/giuliomachado/klimatus/FavoriteActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FavoritesActivity extends AppCompatActivity {
+public class FavoriteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favorites);
+        setContentView(R.layout.activity_favorite);
     }
 }
