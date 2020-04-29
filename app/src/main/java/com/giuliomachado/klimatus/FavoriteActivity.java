3254e394f16cccc9b48c1d2cf7b92769c42c12b1
Giuliomachado <giuliocfmachado@gmail.com>
2020-04-28 22:34:56 -0300

    Versão 1.3 28/04/2020 Intent entre as páginas Ajuste da imagem da cidade Padronização dos IDs

--- app/src/main/java/com/giuliomachado/klimatus/FavoriteActivity.java
+++ app/src/main/java/com/giuliomachado/klimatus/FavoriteActivity.java
@@ -2,13 +2,30 @@ package com.giuliomachado.klimatus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class FavoriteActivity extends AppCompatActivity {
+
+    Button btnReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+
+        btnReturn = findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FavoriteActivity.this, MainActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/giuliomachado/klimatus/MainActivity.java
+++ app/src/main/java/com/giuliomachado/klimatus/MainActivity.java
@@ -2,13 +2,30 @@ package com.giuliomachado.klimatus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCityFav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Esconder barra de navegação
+        getSupportActionBar().hide();
+
+        btnCityFav = findViewById(R.id.btnReturn);
+        btnCityFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FavoriteActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
